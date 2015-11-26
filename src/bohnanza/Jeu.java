5f3a237a7b971de043877bd5af68bb1b41772a08
Thomas Daloz <thomasdaloz@gmail.com>
2015-11-26 15:14:00 +0100

    Création de la liste de carte dans Jeu avec shuffle.

---
+++ src/bohnanza/Jeu.java
@@ -0,0 +1,48 @@
+package bohnanza;
+
+import bohnanza.Carte.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Jeu {
+
+    private List<Carte> listeCarte;
+
+    public Jeu(){
+        initListeCarte();
+    }
+
+    private void initListeCarte() {
+        listeCarte = new ArrayList<>();
+        for (int i = 0; i < 104; i++) {
+            if (i<6){
+                listeCarte.add(new Carte_Pata_Tecktonik());
+            }
+            else if(i<14){
+                listeCarte.add(new Carte_Pata_Terroriste());
+            }
+            else if(i<24){
+                listeCarte.add(new Carte_Pata_Tetraplegique());
+            }
+            else if(i<36){
+                listeCarte.add(new Carte_Pata_Tetenucleaire());
+            }
+            else if(i<50){
+                listeCarte.add(new Carte_Pata_Testosterone());
+            }
+            else if(i<66){
+                listeCarte.add(new Carte_Pata_Tequilla());
+            }
+            else if(i<84){
+                listeCarte.add(new Carte_Pata_Tentacule());
+            }
+            else {
+                listeCarte.add(new Carte_Pata_Twerk());
+            }
+        }
+
+        Collections.shuffle(listeCarte);
+    }
+}
